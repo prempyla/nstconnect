197978d1b02f0ee68e11d21cf672108d953f6036
prempyla <pylaprem@gmail.com>
2025-04-19 19:43:26 +0530

    Initial commit: Added landing page

---
+++ src/components/FeatureCard.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from './FeatureCard.module.css';
+
+export default function FeatureCard({ title, actionText, customClass, isActive = false }) {
+  const href = `/${title.toLowerCase()}`;
+  
+  return (
+    <Link 
+      href={href}
+      className={`${styles.card} ${customClass} ${isActive ? styles.active : ''}`}
+    >
+      <div className={styles.actionText}>
+        {actionText}
+      </div>
+      
+      <div className={styles.title}>
+        {title}
+      </div>
+      
+      <div className={styles.arrow}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+        </svg>
+      </div>
+    </Link>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import styles from './NavBar.module.css';
+
+export default function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <Link href="/" className={styles.logo}>
+          <div className={styles.logoCircle}></div>
+          <div className={styles.logoText}>NST Connect</div>
+        </Link>
+        
+        <div className={styles.navLinks}>
+          <Link href="/confessions" className={styles.navLink}>
+            CONFESSIONS
+          </Link>
+          <Link href="/challenges" className={styles.navLink}>
+            CHALLENGES
+          </Link>
+          <Link href="/safe-rooms" className={styles.navLink}>
+            SAFE ROOMS
+          </Link>
+        </div>
+        
+        <Link href="/signup" className={styles.signupButton}>
+          SIGN UP
+        </Link>
+      </div>
+    </nav>
+  );
+}
