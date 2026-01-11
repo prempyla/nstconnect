499ad43a740152ce8637dd90fbaa69ad0e5cc750
prempyla <pylaprem@gmail.com>
2026-01-11 12:00:00 +0530

    feat(frontend): navbar component

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import styles from './NavBar.module.css';
+
+export default function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <Link href="/" className={styles.logo}>
+          <div className={styles.logoContainer}>
+            <span className={styles.logoCircle}></span>
+            <span className={styles.logoText}>NST</span>
+            <span className={styles.connectText}>Connect</span>
+          </div>
+        </Link>
+        <div className={styles.navLinks}>
+          <Link href="/confessions" className={styles.navLink}>
+            CONFESSIONS
+          </Link>
+          <Link href="/challenges" className={styles.navLink}>
+            CHALLENGES
+          </Link>
+          {/* Temporarily hidden - coming soon */}
+          {/* <Link href="/safe-rooms" className={styles.navLink}>
+            SAFE ROOMS
+          </Link> */}
+        </div>
+      </div>
+    </nav>
+  );
+}
